38af4b4cde569b0d808f74333223b49053550a4b
Daniel Corcoran <danielc.92@outlook.com>
2019-08-15 15:56:55 +1000

    Added connection handler

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/jobboard', { useNewUrlParser: true })
 
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'))
+db.once('open', function() {
+    console.log('Successfully connected to MongoDB on localhost.')
+})
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3001;
 
 app.listen(port, ()=> {
     console.log(`Listening on port ${port}`)
