5686e28bdb7fc9c3bf70cc5fdbfe37dd3af05d6d
Daniel Corcoran <danielc.92@outlook.com>
2019-08-15 21:55:58 +1000

    Added skill router

---
+++ routes/skill.route.js
@@ -0,0 +1,19 @@
+const Skill = require('../models/skill.model');
+const mongoose = require('mongoose');
+const router = mongoose.Router();
+
+router.get('/', (request, response) => {
+    Skill.find()
+        .then(result => response.status(200).json(result))
+        .catch(error => response.status(400).json({ error }))
+})
+
+router.post('/', (request, response) => {
+    const new_item = new Skill(request.body)
+
+    new_item.save()
+        .then(result => response.status(200).json(result))
+        .catch(error => response.status(400).json({ error }))
+})
+
+module.exports = router;
