89227dc8e4115784cceb9bbf24d339b91846a688
Daniel Corcoran <danielc.92@outlook.com>
2019-08-16 16:43:23 +1000

    Created an application model

---
+++ models/application.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const JobApplicationSchema = mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    job: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Job',
+        required: true
+    }
+},
+{
+    timestamps: true
+})
+
+module.exports = mongoose.model('JobApplication', ApplicationSchema)
