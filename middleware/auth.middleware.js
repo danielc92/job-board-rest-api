7ff04f73aa53c7a8f6f3218c32268571d4ce7711
Daniel Corcoran <danielc.92@outlook.com>
2019-08-17 12:50:43 +1000

    Added auth middleware

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const settings = require('../settings');
+
+module.exports = function(request, response, next) {
+
+    // Grab the token from header
+    const token = request.headers['x-access-token'] || request.headers['authorization'];
+
+    // Check if token exists
+    if (!token) {
+        return response.status(401).json({message: "Access denied, no token was provided in headers (x-access-token or authorization)."})
+    }
+
+    try {
+        const decoded = jwt.verify(token, settings.token_secret);
+        request.user = decoded;
+        next();
+    } catch (ex) {
+        response.status(400).json({message: "Invalid Token, try again."})
+    }
+}
