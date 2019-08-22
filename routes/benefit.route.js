d82a3096554a1d5755ccd30da9223e27e1af8bcc
Daniel Corcoran <danielc.92@outlook.com>
2019-08-22 19:24:15 +1000

    Added get and post for Benefit router

---
+++ routes/benefit.route.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const Benefit = require('../models/benefit.model');
+const router = express.Router();
+
+router.get('/', (request, response) => {
+    Benefit.find({})
+        .then(results => response.status(200).json(results))
+        .catch(error => response.status(400).json({ error }))
+})
+
+router.post('/', (request, response) => {
+    
+    const new_item = new Benefit(request.body)
+    
+    new_item.save()
+        .then(result => response.status(200).json(result))
+        .catch(error => response.status(400).json({ error }))  
+})
+
+module.exports = router;
