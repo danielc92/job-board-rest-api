a36b864080d6d8bc513b8d37f0328d86eea80672
Daniel Corcoran <danielc.92@outlook.com>
2019-08-22 19:20:16 +1000

    Added name field and timestamps to benefit schema

---
+++ models/benefit.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const BenefitSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true
+    }
+},
+{
+    timestamps: true
+})
+
+module.exports = mongoose.model('Benefit', BenefitSchema)
