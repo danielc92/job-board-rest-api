68cd4fe983ae0c6a1da145b70b0fd6f9e0a233fb
Daniel Corcoran <danielc.92@outlook.com>
2019-09-07 13:57:05 +1000

    Added reverse sort on job list from job route on createdAt timestamp

--- routes/job.route.js
+++ routes/job.route.js
@@ -8,6 +8,7 @@ router.get('/list', (request, response) => {
 
     Job.find()
         .select('title')
+        .sort({ createdAt: -1})
         .then(results => response.status(200).json({ results }))
         .catch(error => response.status(400).json({ error }))
 })
