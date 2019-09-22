fbcebe3c422589d526a4daee151ac36c1b2b43bf
daniel corcoran <danielc.92@outlook.com>
2019-09-22 12:41:09 +1000

    Added post for category

--- routes/category.route.js
+++ routes/category.route.js
@@ -10,4 +10,25 @@ router.get('/', authMiddleware, (request, response) => {
         .catch(error => response.status(400).json({ error }))
 })
 
+router.post('/', async (request, response) => {
+    
+    const { name } = request.body;
+
+    // Check if Category exists and return response if so.
+    let exists = await Category.findOne({ name })
+    if (exists) return response.status(400).json({ error: "Category already exists."})
+    
+    // Create new Category object and attempt to save
+    let category = new Category({ name })
+
+    let result = await category.save()
+        .then(result=>result)
+        .catch(error=>error)
+    
+    // Check for validation error, else naively return status 200
+    if (result.name === 'ValidationError') return response.status(400).json({error: "Validation error", result})
+
+    return response.status(200).json({ message: `'${name}' Category has been successfully created.`})
+    
+})
 module.exports = router;
