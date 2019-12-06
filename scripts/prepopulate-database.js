9bbb93f9cf36ea8cc9e69eb15978b958a6121aa4
daniel corcoran <danielc.92@outlook.com>
2019-12-06 22:25:25 +1100

    Completed prepopulating script for mongodb, added more scripts to package.json for convenience

--- scripts/prepopulate-database.js
+++ scripts/prepopulate-database.js
@@ -11,6 +11,7 @@ db.once('open', function() {
     console.log(`Successfully connected to MongoDB on ${uri}.`);
 })
 
+
 // Import models
 const Skill = require('../models/skill.model');
 const User = require('../models/user.model');
@@ -18,6 +19,49 @@ const Benefit = require('../models/benefit.model');
 const Category = require('../models/category.model');
 
 
+// Import Dummy data
+const skillsData = require('../data/skills.json');
+const categoriesData = require('../data/categories.json');
+const benefitsData = require('../data/benefits.json');
 
-//
 
+// Inject Dummy data
+skillsData.map(name => {
+    Skill.create({
+        name
+    }).then(result => {
+        console.log('Successfully inserted: ', result);
+    }).catch(error => {
+        console.log(error)
+    })
+})
+
+categoriesData.map(name => {
+    Category.create({
+        name
+    }).then(result => {
+        console.log('Successfully inserted: ', result);
+    }).catch(error => {
+        console.log(error)
+    })
+})
+
+benefitsData.map(name => {
+    Benefit.create({
+        name
+    }).then(result => {
+        console.log('Successfully inserted: ', result);
+    }).catch(error => {
+        console.log(error)
+    })
+})
+
+
+User.create({
+    email: "test@test.com",
+    password: "123456789",
+    first_name: "john",
+    last_name: "doe",
+})
+.then(result => console.log(result))
+.catch(error => console.log(error))
