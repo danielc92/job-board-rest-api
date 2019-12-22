1484b432f42a4728c15741a41c3aec42b6e6145a
daniel corcoran <danielc.92@outlook.com>
2019-12-22 14:40:31 +1100

    Added condition in location route

--- routes/location.route.js
+++ routes/location.route.js
@@ -6,8 +6,8 @@ const limit = 16;
 
 router.get('/', authMiddleware, (request, response) => {
     const { search } = request.query;
-    console.log('Searching for ', search)
-    Location.find({ "$text": { "$search": search }})
+    const query = search ? { "$text": { "$search": search }} : {}
+    Location.find(query)
     .select('location location_string state postcode locality -_id')
     .limit(limit)
     .then(results => response.status(200).json(results))
