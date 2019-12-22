1a61dfcb9aa1f4d124170ee39dbe843b35a8a278
daniel corcoran <danielc.92@outlook.com>
2019-12-22 14:36:27 +1100

    Added full text search to location route

--- routes/location.route.js
+++ routes/location.route.js
@@ -5,7 +5,9 @@ const authMiddleware = require('../middleware/auth.middleware');
 const limit = 16;
 
 router.get('/', authMiddleware, (request, response) => {
-    Location.find({})
+    const { search } = request.query;
+    console.log('Searching for ', search)
+    Location.find({ "$text": { "$search": search }})
     .select('location location_string state postcode locality -_id')
     .limit(limit)
     .then(results => response.status(200).json(results))
