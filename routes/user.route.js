bea095b5c9c96cb3d1be38ddec8d4fd3f3b7ce3f
daniel corcoran <danielc.92@outlook.com>
2019-12-23 20:29:45 +1100

    Removed unwanted user fetch

--- routes/user.route.js
+++ routes/user.route.js
@@ -62,10 +62,5 @@ router.post('/login', async (request, response) => {
 
 })
 
-router.get('/', (request, response) => {
-    User.find()
-        .then(results => response.status(200).json({ results }))
-        .catch(error => response.status(400).json({ error }))
-})
 
 module.exports = router;
