9eaa200fcfb1a7511a5693c31a1eb017ab25a55c
daniel corcoran <danielc.92@outlook.com>
2020-01-04 15:01:56 +1100

    Id catch

--- routes/application.route.js
+++ routes/application.route.js
@@ -3,27 +3,32 @@ const User = require('../models/user.model');
 const Job = require('../models/job.model');
 const express = require('express');
 const router = express.Router();
-const ObjectId = require('mongoose').Types.ObjectId;
+// const {} = 
 
 router.post('/', async (request, response) => {
 
     // First store the request body
     const { applicant_id, job_id } = request.body;
 
-    let userExists = await User.findOne({ _id: applicant_id })
-    if (!userExists) return response.status(400).json({ error: "User does not exist or invalid"})
-
-    let jobExists = await Job.findOne({ _id: job_id })
-    if (!jobExists) return response.status(400).json({ error: "Job does not exist or invalid"})
+    try {
+        let userExists = await User.findOne({ _id: applicant_id })
+        if (!userExists) return response.status(400).json({ error: "User does not exist"})
     
-    let applicationExists = await JobApplication.findOne({ applicant_id, job_id})
-    console.log(applicationExists, 'this is application')
-    if (applicationExists) return response.status(400).json({ error: "You've already applied for this job."})
+        let jobExists = await Job.findOne({ _id: job_id })
+        if (!jobExists) return response.status(400).json({ error: "Job does not exist"})
+    
+        let applicationExists = await JobApplication.findOne({ applicant_id, job_id})
+        if (applicationExists) return response.status(400).json({ error: "You've already applied for this job."})
+    }
+    catch {
+        return response.status(400).json({ error: "Invalid id."})
+    }
+   
 
     const application = new JobApplication({
         applicant_id, job_id
     })
-
+    
     application.save()
         .then(result => response.status(200).json({ result }))
         .catch(error => response.status(400).json({ error }))
