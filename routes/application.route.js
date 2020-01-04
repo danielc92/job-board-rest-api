3426edb7d0859c96cf36d48d2c184242250038e3
daniel corcoran <danielc.92@outlook.com>
2020-01-04 14:49:52 +1100

    Extra validation on application route.

--- routes/application.route.js
+++ routes/application.route.js
@@ -1,22 +1,26 @@
-const Application = require('../models/application.model');
+const JobApplication = require('../models/application.model');
 const User = require('../models/user.model');
 const Job = require('../models/job.model');
 const express = require('express');
 const router = express.Router();
-
+const ObjectId = require('mongoose').Types.ObjectId;
 
 router.post('/', async (request, response) => {
 
     // First store the request body
     const { applicant_id, job_id } = request.body;
 
-    let userExists = User.findOne({ _id: applicant_id })
+    let userExists = await User.findOne({ _id: applicant_id })
     if (!userExists) return response.status(400).json({ error: "User does not exist or invalid"})
 
-    let jobExists = Job.findOne({ _id: job_id })
+    let jobExists = await Job.findOne({ _id: job_id })
     if (!jobExists) return response.status(400).json({ error: "Job does not exist or invalid"})
     
-    const application = new Application({
+    let applicationExists = await JobApplication.findOne({ applicant_id, job_id})
+    console.log(applicationExists, 'this is application')
+    if (applicationExists) return response.status(400).json({ error: "You've already applied for this job."})
+
+    const application = new JobApplication({
         applicant_id, job_id
     })
 
@@ -26,7 +30,7 @@ router.post('/', async (request, response) => {
 })
 
 router.get('/', (request, response) => {
-    Application.find()
+    JobApplication.find()
         .populate("applicant_id job_id")
         .then(results => response.status(200).json({ results }))
         .catch(error => response.status(400).json({ error }))
@@ -34,8 +38,13 @@ router.get('/', (request, response) => {
 
 router.get('/list', (request, response) => {
 
-    const { applicant_id, job_id } = request.query;
-    Application.find({ applicant_id, job_id })
+    const { applicant_id } = request.query;
+    JobApplication.find({ applicant_id})
+        .populate({
+            path: 'job_id',
+            select: 'title'
+        })
+        .sort('-createdAt')
         .then(results => response.status(200).json({ results }))
         .catch(error => response.status(400).json({ error }))
 })
