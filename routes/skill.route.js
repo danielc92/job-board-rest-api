ebf52cf4924b0d7ef09119ad92cc9243a4d5172d
Daniel Corcoran <danielc.92@outlook.com>
2020-03-03 19:35:58 +1100

    Added cacheing strat for skillls

--- routes/skill.route.js
+++ routes/skill.route.js
@@ -3,15 +3,25 @@ const express = require('express')
 const router = express.Router()
 const authMiddleware = require('../middleware/auth.middleware')
 const select = require('../constants/select')
+const {ONE_MINUTE} = require('../constants/ttl')
+const client = require('../redis-client')
 
 /*
 Get skill list
 */
 router.get('/list', authMiddleware, (request, response) => {
-    Skill.find()
-        .select(select.GET_SKILL)
-        .then(result => response.status(200).json(result))
-        .catch(error => response.status(400).json({ error }))
+    client.get('skill:list', function (err, reply) {
+        if (reply) return response.status(200).json(JSON.parse(reply))
+        Skill.find()
+            .select(select.GET_SKILL)
+            .sort('name')
+            .lean()
+            .then(results => {
+                client.setex('skill:list', ONE_MINUTE, JSON.stringify(results))
+                return response.status(200).json(results)
+            })
+            .catch(error => response.status(400).json({ error }))
+    })
 })
 
 module.exports = router
