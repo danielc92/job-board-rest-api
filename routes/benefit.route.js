78e11f34c6202156b648d7fca0fc9363bff677fe
Daniel Corcoran <danielc.92@outlook.com>
2020-03-03 19:32:33 +1100

    Added cache strategy for benefits

--- routes/benefit.route.js
+++ routes/benefit.route.js
@@ -1,15 +1,25 @@
 const express = require('express')
 const Benefit = require('../models/benefit.model')
 const router = express.Router()
-const authMiddleware = require('../middleware/auth.middleware')
+const {ONE_MINUTE} = require('../constants/ttl')
+const client = require('../redis-client')
 
 /*
 Get benefits list
 */
 router.get('/list', (request, response) => {
-    Benefit.find({})
-        .then(results => response.status(200).json(results))
-        .catch(error => response.status(400).json({ error }))
+    client.get('ben:list', function (err, reply) {
+        if (reply) return response.status(200).json({results: JSON.parse(reply)})
+        Benefit.find()
+            .select('name')
+            .sort('name')
+            .lean()
+            .then(results => {
+                client.setex('ben:list', ONE_MINUTE, JSON.stringify(results))
+                return response.status(200).json({results})
+            })
+            .catch(error => response.status(400).json({ error }))
+    })
 })
 
 module.exports = router
