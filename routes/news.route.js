2657f47bc75df7bd106407d4e92c1c4157a8d8c3
Daniel Corcoran <danielc.92@outlook.com>
2020-03-03 19:56:57 +1100

    Added news list cacheing strategy

--- routes/news.route.js
+++ routes/news.route.js
@@ -3,19 +3,31 @@ const router = express.Router()
 const News = require('../models/news.model')
 const limit = require('../constants/limit')
 const select = require('../constants/select')
+const client = require('../redis-client')
+const {ONE_HOUR} = require('../constants/ttl')
 
 router.get('/list', (request, response) => {
     const { page } = request.query
     let options = {
         select: select.GET_NEWS_LIST,
         sort: { createdAt: 'desc' },
+        lean: true,
         limit: limit.NEWS_LIST,
+        page: page ? page : 1
     }
 
-    if (page) options.page = page 
-    News.paginate({}, options)
-        .then(results => response.status(200).json({ results }))
-        .catch(error => response.status(400).json({ error: 'An error occured. Could not find news list.' }))
+    const key = `news:list:p${options.page}`
+
+    client.get(key, function(err, reply) {
+        if (reply) return response.status(200).json({ results: JSON.parse(reply)})
+
+        News.paginate({}, options)
+            .then(results => {
+                client.setex(key, ONE_HOUR, JSON.stringify(results))
+                return response.status(200).json({ results })
+            })
+            .catch(error => response.status(400).json({ error: 'An error occured. Could not find news list.' }))
+    })
 })
 
 router.get('/', (request, response) => {
