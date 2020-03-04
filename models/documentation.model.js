b29f40a96d5308ad7b0c3f0a3218d7b461bf6103
Daniel Corcoran <danielc.92@outlook.com>
2020-03-04 21:13:16 +1100

    Added documentation model

---
+++ models/documentation.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const DocumentationSchema = mongoose.Schema({
+    title: {
+        type:String,
+        required:true,
+    },
+    route: {
+        type:String,
+        required:true,
+    },
+    content: {
+        type: [Object],
+        required:true,
+    }
+},
+{
+    timestamps: true
+})
+
+const Documentation = mongoose.model('Documentation', DocumentationSchema)
+
+module.exports = Documentation
