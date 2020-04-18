0ac03896a297cd84fa7c838ce6bc5f9f238d19e8
Daniel Corcoran <danielc.92@outlook.com>
2020-04-18 14:20:48 +1000

    Added env vars to redis client

--- redis-client/index.js
+++ redis-client/index.js
@@ -1,8 +1,15 @@
-const redis = require('redis')
-const client =redis.createClient()
+const redis = require("redis")
+const client = redis.createClient(
+  process.env.REDIS_PORT,
+  process.env.REDIS_HOST
+)
 
-client.on('error', function(error) {
-    console.error(error)
+client.on("error", function (error) {
+  console.error(error)
+})
+
+client.on("connect", function () {
+  console.log("Successfully connected to redis client.")
 })
 
 module.exports = client
