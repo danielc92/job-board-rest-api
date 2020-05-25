7ac8c3f694ac2879ce1cc94207270fffb0378a40
Daniel Corcoran <danielc.92@outlook.com>
2020-05-25 19:44:09 +1000

    Added _id

--- constants/select/index.js
+++ constants/select/index.js
@@ -4,11 +4,11 @@ module.exports = {
   GET_JOB_LIST_SEEKER:
     "title job_summary salary_range_low salary_range_high slug createdAt category",
   GET_JOB: "-__v -location -creator_id -updatedAt",
-  GET_LOCATION: "location location_string state postcode locality -_id",
+  GET_LOCATION: "location location_string state postcode locality",
   GET_NEWS_LIST: "title summary category createdAt slug",
   GET_PROFILE:
     "summary skills experience education achievements available phone -_id",
   GET_SKILL: "name",
   GET_USER: "-password -admin",
-  GET_APPLICATION_LIST_EMPLOYER: "first_name last_name email"
+  GET_APPLICATION_LIST_EMPLOYER: "first_name last_name email",
 }
