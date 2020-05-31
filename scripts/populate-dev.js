7a9d17b2e8787310d179f99c256f7d61b44ca725
Daniel Corcoran <danielc.92@outlook.com>
2020-05-31 12:08:48 +1000

    Modified dev script

--- scripts/populate-dev.js
+++ scripts/populate-dev.js
@@ -14,6 +14,7 @@ const faker = require("faker")
 const CareerStats = require("../models/career_stats.model")
 const News = require("../models/news.model")
 const User = require("../models/user.model")
+const JobApplication = require("../models/application.model")
 const Job = require("../models/job.model")
 const skillsList = require("../data/skills.json")
 const categoriesList = require("../data/categories.json")
@@ -40,84 +41,123 @@ db.once("open", function () {
   console.log(`Successfully connected to MongoDB on ${uri}.`)
 })
 
-const insertEmployer = () => {
-  let user = {
-    email: "test@test.com",
-    password: Utils.getPassword(),
-    first_name: faker.name.firstName(),
-    last_name: faker.name.lastName(),
-    is_employer: true,
-    activated: true,
+const insertEmployers = async () => {
+  try {
+    let employers = new Array(10).fill(0).map((e, index) => ({
+      email: `test-employer-${index}@applymer-test.com`,
+      password: Utils.getPassword(),
+      first_name: faker.name.firstName(),
+      last_name: faker.name.lastName(),
+      is_employer: true,
+      activated: true,
+    }))
+
+    const userResults = await User.insertMany(employers)
+
+    let employerProfiles = userResults.map((u) => ({ user_id: u._id }))
+    await CareerStats.insertMany(employerProfiles)
+
+    let jobs = new Array(100).fill(null).map((item) => {
+      const randomLocation = Utils.randomItemFromArray(locationList)
+      const randomTitle = Utils.randomItemFromArray(titlesList)
+      const jobItem = {
+        creator_id: faker.random.arrayElement(userResults)._id,
+        category: Utils.randomItemFromArray(categoriesList)
+          .trim()
+          .toLowerCase(),
+        benefits: Utils.randomItemsFromArray(benefitsList),
+        company_name: faker.name.firstName() + " ltd corp",
+        company_summary: faker.lorem.paragraph(),
+        contact_summary: faker.lorem.paragraph(),
+        employment_type: Utils.randomItemFromArray(employment_types),
+        job_summary: faker.lorem.paragraph(),
+        location_string: randomLocation.location_string,
+        location: randomLocation.location,
+        requirements: [],
+        salary_range_high: Utils.randomHighSalary(),
+        salary_range_low: Utils.randomLowSalary(),
+        skills: Utils.randomItemsFromArray(skillsList),
+        slug: Helpers.slugify(randomTitle),
+        title: randomTitle,
+      }
+      return jobItem
+    })
+
+    await Job.insertMany(jobs)
+    console.log("[SUCCESS] Employers and jobs.")
+  } catch (error) {
+    console.log("[ERROR] ", error)
   }
-  User.create(user)
-    .then((result) => {
-      CareerStats.create({ user_id: result._id })
-        .then((results) =>
-          console.log("[SUCCESS] Created profile for employer")
-        )
-        .catch((error) =>
-          console.log("[ERROR] Failed to create employer profile")
-        )
-
-      let jobData = new Array(1000).fill(null).map((item) => {
-        const randomLocation = Utils.randomItemFromArray(locationList)
-        const randomTitle = Utils.randomItemFromArray(titlesList)
-        const jobItem = {
-          creator_id: result._id,
-          category: Utils.randomItemFromArray(categoriesList)
-            .trim()
-            .toLowerCase(),
-          benefits: Utils.randomItemsFromArray(benefitsList),
-          company_name: faker.name.firstName() + " ltd corp",
-          company_summary: faker.lorem.paragraph(),
-          contact_summary: faker.lorem.paragraph(),
-          employment_type: Utils.randomItemFromArray(employment_types),
-          job_summary: faker.lorem.paragraph(),
-          location_string: randomLocation.location_string,
-          location: randomLocation.location,
-          requirements: [],
-          salary_range_high: Utils.randomHighSalary(),
-          salary_range_low: Utils.randomLowSalary(),
-          skills: Utils.randomItemsFromArray(skillsList),
-          slug: Helpers.slugify(randomTitle),
-          title: randomTitle,
-        }
-        return jobItem
-      })
-
-      Job.insertMany(jobData)
-        .then((result) => console.log("[SUCCESS] Inserted jobs"))
-        .catch((error) => console.log("[ERROR] Failed to insert jobs", error))
-    })
-    .catch((error) => console.log(error))
-  return
 }
 
-const insertJobSeekers = () => {
-  const jobSeekers = new Array(25).fill(true).map((item, index) => ({
-    email: `test${index}@test.com`,
-    password: Utils.getPassword(),
-    first_name: faker.name.firstName(),
-    last_name: faker.name.lastName(),
-    activated: true,
-  }))
+const insertJobSeekers = async () => {
+  try {
+    const jobSeekers = new Array(50).fill(true).map((item, index) => ({
+      email: `test-seeker-${index}@applymer-test.com`,
+      password: Utils.getPassword(),
+      first_name: faker.name.firstName(),
+      last_name: faker.name.lastName(),
+      activated: true,
+    }))
 
-  User.insertMany(jobSeekers)
-    .then((results) => {
-      console.log("[SUCCESS] Inserted job seekers")
-      const careerStats = results.map((r) => ({ user_id: r._id }))
-      CareerStats.insertMany(careerStats)
-        .then((results) => console.log("[SUCCESS] Inserted profiles"))
-        .catch((error) => console.log("[ERROR] Failed to insert profiles"))
-    })
-    .catch((error) =>
-      console.log("[ERROR] Failed to insert job seekers", error)
-    )
-  return
+    const userResults = await User.insertMany(jobSeekers)
+
+    let seekerProfiles = userResults.map((u) => ({ user_id: u._id }))
+    await CareerStats.insertMany(seekerProfiles)
+    console.log("[SUCCESS] Inserted job seekers with profiles.")
+  } catch (error) {
+    console.log("[ERROR] ", error)
+  }
 }
 
-const insertNews = () => {
-  const newsItems = new Array(30).fill(true).map((item) => {
+const insertApplications = async () => {
+  try {
+    const seeker1 = await User.findOne({
+      email: "test-seeker-1@applymer-test.com",
+    })
+
+    const seeker2 = await User.findOne({
+      email: "test-seeker-2@applymer-test.com",
+    })
+
+    const seeker3 = await User.findOne({
+      email: "test-seeker-3@applymer-test.com",
+    })
+
+    const jobs = await Job.find().limit(90)
+
+    const applications = jobs.map((j) => ({
+      job_id: j._id,
+      applicant_id: seeker1._id,
+      user_message: faker.random.words(30),
+    }))
+
+    const applications2 = jobs.map((j) => ({
+      job_id: j._id,
+      applicant_id: seeker2._id,
+      user_message: faker.random.words(30),
+    }))
+
+    const applications3 = jobs.map((j) => ({
+      job_id: j._id,
+      applicant_id: seeker3._id,
+      user_message: faker.random.words(30),
+    }))
+
+    await JobApplication.insertMany([
+      ...applications,
+      ...applications2,
+      ...applications3,
+    ])
+
+    console.log("[SUCCESS] Inserted applications.")
+  } catch (error) {
+    console.log("[ERROR] ", error)
+  }
+}
+
+const insertNews = async () => {
+  const newsItems = new Array(30).fill(0).map((item) => {
     const title = faker.lorem.text().substring(0, 20)
     const slug = Helpers.slugify(title)
     const content = [
@@ -160,13 +200,14 @@ const insertNews = () => {
     }
   })
 
-  News.insertMany(newsItems)
-    .then((result) => console.log("[SUCCESS] Inserted news"))
-    .catch((error) => console.log("[ERROR] Failed to insert news", error))
+  await News.insertMany(newsItems)
 
-  return
+  console.log("[SUCCESS] Inserted News.")
 }
 
-insertEmployer()
-insertJobSeekers()
-insertNews()
+;(async function () {
+  await insertEmployers()
+  await insertJobSeekers()
+  await insertApplications()
+  await insertNews()
+})()
