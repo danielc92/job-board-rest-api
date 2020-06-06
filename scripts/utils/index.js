58a016e1d41027f19ba47f5f0d3ade23656d339b
Daniel Corcoran <danielc.92@outlook.com>
2020-06-06 10:46:05 +1000

    Deleted randomText()

--- scripts/utils/index.js
+++ scripts/utils/index.js
@@ -58,6 +58,4 @@ module.exports = {
   randomHighSalary: () => Math.floor(Math.random() * 250000),
   getPassword: () =>
     "$2b$10$U28Rw6QVdclrP66rg/PJquwegIOlCTSckx93RDW8EY.GiyZw2Kedq",
-  randomText: () =>
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 }
