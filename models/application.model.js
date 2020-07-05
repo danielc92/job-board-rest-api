75ac5a554706e3bbcd99a6d92eddb2539ed496ec
Daniel Corcoran <danielc.92@outlook.com>
2020-07-05 17:23:31 +1000

    Upped limit

--- models/application.model.js
+++ models/application.model.js
@@ -6,33 +6,33 @@ const JobApplicationSchema = mongoose.Schema(
     applicant_id: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: true
+      required: true,
     },
     job_id: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Job",
-      required: true
+      required: true,
     },
     status: {
       type: String,
       default: "pending",
       trim: true,
       lowercase: true,
-      enum: ["pending", "rejected", "interested", "withdrawn"]
+      enum: ["pending", "rejected", "interested", "withdrawn"],
     },
     user_message: {
       type: String,
       default: null,
       trim: true,
-      maxlength: 1000
+      maxlength: 3000,
     },
     rating: {
       type: Number,
-      default: 0
-    }
+      default: 0,
+    },
   },
   {
-    timestamps: true
+    timestamps: true,
   }
 )
 
